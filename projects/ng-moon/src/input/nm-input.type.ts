a7505651ebaeac8b2b544565c8482666af7059e1
ng-nest-moon <ngnest@163.com>
2019-04-07 23:34:21 +0800

    update: input component spec.ts

--- projects/ng-moon/src/input/nm-input.type.ts
+++ projects/ng-moon/src/input/nm-input.type.ts
@@ -1,30 +1,28 @@
 export const prefix = "nm-input";
-export enum InputTypeEnum {
-  Text = "text",
-  Password = "password",
-  Number = "number"
-}
-export enum InputLayoutEnum {
-  Horizontal = "horizontal",
-  Vertical = "vertical"
-}
-export enum InputIconLayoutEnum {
-  Left = "left",
-  Right = "right"
-}
-export enum InputSizeEnum {
-  Large = "large",
-  Small = "small"
-}
+
 export interface InputOption {
   layout?: InputLayoutEnum;
   label?: string;
   type?: InputTypeEnum;
-  size?: InputSizeEnum;
   placeholder?: string;
   required?: boolean;
   disabled?: boolean;
   icon?: string;
   iconLayout?: InputIconLayoutEnum;
-  key?: string;
+}
+
+export enum InputLayoutEnum {
+  Horizontal = "horizontal",
+  Vertical = "vertical"
+}
+
+export enum InputTypeEnum {
+  Text = "text",
+  Password = "password",
+  Number = "number"
+}
+
+export enum InputIconLayoutEnum {
+  Left = "left",
+  Right = "right"
 }
