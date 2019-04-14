27639914a9bb49e74583795ffb93f012c1fc5809
ng-moon <ngmoon2019@163.com>
2019-04-14 12:39:50 +0800

    update: anchor component

--- src/share/anchor/anchor.component.ts
+++ src/share/anchor/anchor.component.ts
@@ -7,10 +7,12 @@ import {
   HostBinding,
   Renderer2,
   ChangeDetectionStrategy,
-  ChangeDetectorRef
+  ChangeDetectorRef,
+  ViewChild
 } from "@angular/core";
-import { map } from "rxjs/operators";
 import { Location } from "@angular/common";
+import { Router } from "@angular/router";
+import { ScrollDispatcher, CdkScrollable } from "@angular/cdk/scrolling";
 
 @Component({
   selector: "[nm-anchor]",
@@ -23,12 +25,16 @@ export class NmAnchorComponent implements OnInit, AfterViewInit {
   @HostBinding("class.nm-anchor") className() {
     return true;
   }
+  @ViewChild("scrolling") scrollingRef: ElementRef;
   list: NmAnchorNode[];
+  active: number = 0;
   constructor(
     private ele: ElementRef,
     private render: Renderer2,
     private location: Location,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private router: Router,
+    private scrollDispatcher: ScrollDispatcher
   ) {}
 
   ngOnInit() {}
@@ -39,24 +45,48 @@ export class NmAnchorComponent implements OnInit, AfterViewInit {
       this.render.addClass(this.ele.nativeElement, "nm-anchor-open");
       let _list: NmAnchorNode[] = [];
       hList.forEach((x: HTMLElement, i: number) => {
-        this.render.setAttribute(x, "id", `link-${i}`);
+        const link = `link-${i}`;
+        this.render.setAttribute(x, "id", link);
         _list = [
           ..._list,
           {
             id: `${i}`,
             title: x.innerText,
-            href: `${this.location.path()}#link-${i}`
+            left: this.setLeft(x),
+            router: `${this.location.path()}`,
+            link: link
           }
         ];
       });
       this.list = _list;
       this.cdr.detectChanges();
     }
+    this.scrollDispatcher
+      .ancestorScrolled(this.scrollingRef)
+      .subscribe((scrollable: CdkScrollable) => {
+        if (scrollable) {
+          console.log("祖先发生scroll了，來源于：");
+          console.log(scrollable.getElementRef().nativeElement);
+        }
+      });
+  }
+
+  setLeft(element: HTMLElement): number {
+    const eles = ["H1", "H2", "H3", "H4", "H5"];
+    const index = eles.indexOf(element.tagName);
+    return index + 1;
+  }
+
+  nav(item: NmAnchorNode, index: number) {
+    this.active = index;
+    this.router.navigate([item.router], { fragment: item.link });
   }
 }
 
 export interface NmAnchorNode {
   id?: number | string;
-  href?: string;
+  router?: string;
+  link?: string;
+  left?: number;
   title?: string;
 }
