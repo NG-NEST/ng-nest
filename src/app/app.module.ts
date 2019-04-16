763d29fd612e544b2e3faf18df214a5cbf15c066
ng-moon <ngmoon2019@163.com>
2019-04-16 23:35:12 +0800

    add: code highlight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgMoonModule } from 'ng-moon';
-import { ShareModule } from 'src/share/share.module';
-import { MainRoutesModule } from 'src/main/routes.module';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgMoonModule } from "ng-moon";
+import { ShareModule } from "src/share/share.module";
+import { MainRoutesModule } from "src/main/routes.module";
+
+import { HighlightModule } from "ngx-highlightjs";
+
+import xml from "highlight.js/lib/languages/xml";
+import scss from "highlight.js/lib/languages/scss";
+import typescript from "highlight.js/lib/languages/typescript";
+
+export function hljsLanguages() {
+  return [
+    { name: "typescript", func: typescript },
+    { name: "scss", func: scss },
+    { name: "xml", func: xml }
+  ];
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
@@ -20,8 +32,11 @@ import { MainRoutesModule } from 'src/main/routes.module';
     HttpClientModule,
     ShareModule,
     MainRoutesModule,
-    NgMoonModule
+    NgMoonModule,
+    HighlightModule.forRoot({
+      languages: hljsLanguages
+    })
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/main/docs/ui/components/input/input.component.ts
+++ src/main/docs/ui/components/input/input.component.ts
@@ -5,4 +5,47 @@ import { Component, ViewEncapsulation } from "@angular/core";
   templateUrl: "./input.component.html",
   encapsulation: ViewEncapsulation.None
 })
-export class InputComponent {}
+export class InputComponent {
+  typescriptCode: string = `
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Router } from "@angular/router";
+
+@Component({
+    selector: "ns-news",
+    templateUrl: "./news.component.html",
+    styleUrls: ["./news.component.scss"],
+    encapsulation: ViewEncapsulation.None
+})
+export class NewsComponent implements OnInit {
+  constructor(public router: Router) {}
+
+  ngOnInit() {}
+}`;
+  htmlCode: string = `
+<section class="main">
+  <h1 i18n="@@developing">开发中</h1>
+  <p class="text" i18n="@@developing.text">抱歉，这个页面正在设计开发中</p>
+  <p class="link">
+    <a (click)="push()" i18n="@@home">首页</a>
+    <a (click)="back()" i18n="@@back">返回</a>
+  </p>
+</section>`;
+  scssCode: string = `
+  @import "src/styles/index";
+
+  ns-index {
+    > section {
+      > .row {
+        display: flex;
+        justify-content: flex-start;
+        padding: 2rem 0;
+        border-bottom: 1px solid $divider-color;
+        > .content {
+        }
+        &:nth-of-type(1) {
+          padding-top: 0;
+        }
+      }
+    }
+  }`;
+}
