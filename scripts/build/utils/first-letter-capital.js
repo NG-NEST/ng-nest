9cf5faa4341e134353b902a440470117190aedd5
ng-nest-moon <ngnest@163.com>
2019-08-01 22:18:37 +0800

    delete: lgnore files

--- scripts/build/utils/first-letter-capital.js
+++
@@ -1,3 +0,0 @@
-module.exports = function firstLetterCapital(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
--- scripts/build/utils/get-file-name.js
+++
@@ -1,8 +0,0 @@
-module.exports = function getFileName(file) {
-  var pattern = /\.{1}[a-z]{1,}$/;
-  if (pattern.exec(file) !== null) {
-    return file.slice(0, pattern.exec(file).index);
-  } else {
-    return file;
-  }
-};
