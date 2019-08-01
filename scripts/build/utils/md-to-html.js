55fc3a00179592f47247322f3177210ffcfcc61f
ng-moon <ngmoon2019@163.com>
2019-08-01 22:18:37 +0800

    delete: lgnore files

--- scripts/build/utils/md-to-html.js
+++
@@ -1,7 +0,0 @@
-const MD = require("marked");
-const fs = require("fs-extra");
-
-module.exports = function mdToHtml(mdPath) {
-  if (!fs.existsSync(mdPath)) return;
-  return MD(fs.readFileSync(mdPath, "utf8"));
-};
