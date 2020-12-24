6c504a76c8f8bc5f7449e79672300c56f1246b8f
ng-nest-moon <ngnest@163.com>
2020-12-24 20:17:13 +0800

    feat(module:table): add row height adaptation

--- lib/ng-nest/ui/table/table.component.spec.ts
+++ lib/ng-nest/ui/table/table.component.spec.ts
@@ -239,7 +239,17 @@ class TestXTableComponent {
   template: `
     <x-theme showDark></x-theme>
     <div class="row">
-      <x-table [columns]="columns" [data]="data" [size]="1000" [bodyHeight]="420" [adaptionHeight]="200" virtualScroll loading> </x-table>
+      <x-table
+        [rowHeight]="0"
+        [columns]="columns"
+        [data]="data"
+        [size]="1000"
+        [bodyHeight]="420"
+        [adaptionHeight]="200"
+        virtualScroll
+        loading
+      >
+      </x-table>
     </div>
   `,
   styles: [
