f198f25eb41f8231179904132f04b9e15219d2a0
ng-nest-moon <ngnest@163.com>
2021-03-03 08:39:46 +0800

    feat(module:upload): add imgCut style
    #10

--- lib/ng-nest/ui/upload/upload-portal.component.ts
+++ lib/ng-nest/ui/upload/upload-portal.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, ViewChild, ElementRef, Renderer2, ChangeDetectorRef } from '@angular/core';
 import { XUploadNode, XUploadPortalPrefix } from './upload.property';
 
 @Component({
@@ -10,8 +10,29 @@ import { XUploadNode, XUploadPortalPrefix } from './upload.property';
 })
 export class XUploadPortalComponent {
   file: XUploadNode;
+  @ViewChild('imgRef') imgRef: ElementRef;
+  @ViewChild('cutRef') cutRef: ElementRef;
+  ready = false;
+
   closePortal: () => void;
   destroyPortal: () => void;
 
+  constructor(private renderer: Renderer2, private cdr: ChangeDetectorRef) {}
+
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.setCut();
+  }
+
+  setCut() {
+    const width = this.imgRef.nativeElement.clientWidth;
+    const height = this.imgRef.nativeElement.clientHeight;
+    this.renderer.setStyle(this.cutRef.nativeElement, 'width', `${width}px`);
+    this.renderer.setStyle(this.cutRef.nativeElement, 'height', `${height}px`);
+    this.renderer.setStyle(this.imgRef.nativeElement, 'width', `${width}px`);
+    this.renderer.setStyle(this.imgRef.nativeElement, 'height', `${height}px`);
+    this.ready = true;
+    this.cdr.detectChanges();
+  }
 }
