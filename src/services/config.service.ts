f9a7d47b800dcf3491d4a871a3a53a5b551feab1
ng-nest-moon <ngnest@163.com>
2021-11-08 20:41:31 +0800

    chore(package): update package to laster

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -10,7 +10,7 @@ export class ConfigService {
   langs = ['zh_CN', 'en_US'];
   cacheLangs: { [lang: string]: XI18nProperty } = {};
   versions: string[] = [];
-  version = '12.0.8';
+  version = '13.0.0';
   navName = 'NG-NEST';
 
   get lang() {
