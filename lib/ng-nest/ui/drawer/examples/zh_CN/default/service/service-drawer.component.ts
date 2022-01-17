b43b04d5bca0cf9c114d5429de177566b4408b24
ng-nest-moon <ngnest@163.com>
2022-01-17 21:50:09 +0800

    docs(module:drawer): update example code

--- lib/ng-nest/ui/drawer/examples/zh_CN/default/service/service-drawer.component.ts
+++ lib/ng-nest/ui/drawer/examples/zh_CN/default/service/service-drawer.component.ts
@@ -1,15 +1,14 @@
-import { OverlayRef } from '@angular/cdk/overlay';
 import { Component, Inject } from '@angular/core';
-import { X_DRAWER_DATA } from '@ng-nest/ui/drawer';
+import { X_DRAWER_DATA, XDrawerRef } from '@ng-nest/ui/drawer';
 
 @Component({
   selector: 'ex-service-drawer',
   templateUrl: './service-drawer.component.html'
 })
 export class ExServiceDrawerComponent {
-  constructor(@Inject(X_DRAWER_DATA) public data: any, public drawerRef: OverlayRef) {}
+  constructor(@Inject(X_DRAWER_DATA) public data: any, public drawerRef: XDrawerRef<ExServiceDrawerComponent>) {}
 
   close() {
-    this.drawerRef.detach();
+    this.drawerRef.close();
   }
 }
