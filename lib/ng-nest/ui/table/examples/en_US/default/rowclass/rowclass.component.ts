e1919c3086fd56bc8e6aed1d236d91287e97671c
ng-nest-moon <ngnest@163.com>
2022-02-20 21:02:42 +0800

    feat(module:table): add rowClass
    close #181

---
+++ lib/ng-nest/ui/table/examples/en_US/default/rowclass/rowclass.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { RowclassService } from './rowclass.service';
+import { XQuery } from '@ng-nest/ui/core';
+import { XTableColumn, XTableRow } from '@ng-nest/ui/table';
+import { delay } from 'rxjs/operators';
+
+@Component({
+  selector: 'ex-rowclass',
+  templateUrl: './rowclass.component.html',
+  styleUrls: ['./rowclass.component.scss']
+})
+export class ExRowclassComponent {
+  data = (index: number, size: number, query: XQuery) => this.service.getList(index, size, query).pipe(delay(1000));
+  columns: XTableColumn[] = [
+    { id: 'checked', label: '', rowChecked: true, headChecked: true, type: 'checkbox', width: 60 },
+    { id: 'index', label: 'serial', flex: 0.5, left: 0, type: 'index' },
+    { id: 'name', label: 'user', flex: 1.5, sort: true },
+    { id: 'position', label: 'position', flex: 0.5, sort: true },
+    { id: 'email', label: 'mailbox', flex: 1 },
+    { id: 'phone', label: 'phone', flex: 1 },
+    { id: 'organization', label: 'organization', flex: 1, sort: true }
+  ];
+
+  rowClass = (row: XTableRow, index: number) => {
+    return {
+      even: index % 2 === 0,
+      odd: index % 2 === 1,
+      first: index === 0,
+      inspector: row['position'] === 'Director'
+    };
+  };
+
+  constructor(private service: RowclassService) {}
+
+  ngOnInit() {}
+}
--- src/main/test/modules/table/table.module.ts
+++ src/main/test/modules/table/table.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { XTableModule } from '@ng-nest/ui/table';
 import { ExCheckboxComponent } from './checkbox/checkbox.component';
+import { ExRowclassComponent } from './rowclass/rowclass.component';
 import { CheckboxService } from './checkbox/checkbox.service';
 import { ExConfigComponent } from './config/config.component';
 import { ConfigService } from './config/config.service';
+import { RowclassService } from './rowclass/rowclass.service';
 import { TeTableComponent } from './table.component';
 
 const routers = [{ path: '', component: TeTableComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routers), XTableModule],
-  declarations: [TeTableComponent, ExConfigComponent, ExCheckboxComponent],
-  providers: [ConfigService, CheckboxService]
+  declarations: [TeTableComponent, ExConfigComponent, ExCheckboxComponent, ExRowclassComponent],
+  providers: [ConfigService, CheckboxService, RowclassService]
 })
 export class TeTableModule {}
