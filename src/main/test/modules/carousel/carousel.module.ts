5c9bd8ca5c54d5940df43607597dc5dab77c3616
ng-nest-moon <ngnest@163.com>
2022-04-18 19:06:51 +0800

    fix(module:carousel): fix indicator outside style
    close #125

--- src/main/test/modules/carousel/carousel.module.ts
+++ src/main/test/modules/carousel/carousel.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { XCarouselModule } from '@ng-nest/ui/carousel';
 import { ExDefaultComponent } from './default/default.component';
+import { ExCardComponent } from './card/card.component';
 import { TeCarouselComponent } from './carousel.component';
 import { XLayoutModule } from '@ng-nest/ui/layout';
 import { CommonModule } from '@angular/common';
@@ -10,6 +11,6 @@ const routers = [{ path: '', component: TeCarouselComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routers), CommonModule, XCarouselModule, XLayoutModule],
-  declarations: [TeCarouselComponent, ExDefaultComponent]
+  declarations: [TeCarouselComponent, ExDefaultComponent, ExCardComponent]
 })
 export class TeCarouselModule {}
