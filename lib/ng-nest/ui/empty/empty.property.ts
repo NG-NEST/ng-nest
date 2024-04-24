b52d423d782c827c9a0c667edcc284a5ca594c6e
ng-nest-moon <ngnest@163.com>
2024-04-24 21:53:57 +0800

    refactor(module:empty): update input signal

--- lib/ng-nest/ui/empty/empty.property.ts
+++ lib/ng-nest/ui/empty/empty.property.ts
@@ -1,5 +1,5 @@
-import { XTemplate, XWithConfig } from '@ng-nest/ui/core';
-import { Input, Component } from '@angular/core';
+import { XPropertyFunction, XTemplate } from '@ng-nest/ui/core';
+import { Component, input } from '@angular/core';
 
 /**
  * Empty
@@ -7,21 +7,21 @@ import { Input, Component } from '@angular/core';
  * @decorator component
  */
 export const XEmptyPrefix = 'x-empty';
-const X_CONFIG_NAME = 'empty';
+const X_EMPTY_CONFIG_NAME = 'empty';
 
 /**
  * Empty Property
  */
 @Component({ selector: `${XEmptyPrefix}-property`, template: '' })
-export class XEmptyProperty {
+export class XEmptyProperty extends XPropertyFunction(X_EMPTY_CONFIG_NAME) {
   /**
    * @zh_CN 图片地址或自定义模板
    * @en_US Picture address or custom template
    */
-  @Input() @XWithConfig<XTemplate>(X_CONFIG_NAME) img?: XTemplate;
+  img = input<XTemplate>(this.config?.img!);
   /**
    * @zh_CN 内容或自定义模板
    * @en_US Content or custom template
    */
-  @Input() @XWithConfig<XTemplate>(X_CONFIG_NAME) content?: XTemplate;
+  content = input<XTemplate>(this.config?.content!);
 }
