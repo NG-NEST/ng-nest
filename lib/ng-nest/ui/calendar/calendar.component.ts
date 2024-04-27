da615703283de1567d40a06fe594223d281f31c6
ng-nest-moon <ngnest@163.com>
2024-04-27 10:14:20 +0800

    refactor(module:calendar): update rxjs to toSignal

--- lib/ng-nest/ui/calendar/calendar.component.ts
+++ lib/ng-nest/ui/calendar/calendar.component.ts
@@ -1,12 +1,4 @@
-import {
-  Component,
-  ViewEncapsulation,
-  ChangeDetectionStrategy,
-  OnDestroy,
-  inject,
-  computed,
-  signal
-} from '@angular/core';
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, inject, computed, signal } from '@angular/core';
 import { XCalendarPrefix, XCalendarProperty } from './calendar.property';
 import { XIsEmpty } from '@ng-nest/ui/core';
 import { DatePipe, LowerCasePipe, NgClass, NgTemplateOutlet } from '@angular/common';
@@ -16,8 +8,7 @@ import { XDatePickerComponent, XPickerDateComponent, XPickerMonthComponent } fro
 import { XButtonComponent, XButtonsComponent } from '@ng-nest/ui/button';
 import { XRadioComponent } from '@ng-nest/ui/radio';
 import { XTooltipDirective } from '@ng-nest/ui/tooltip';
-import { takeUntil, map } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { FormsModule } from '@angular/forms';
 import { toSignal } from '@angular/core/rxjs-interop';
 import type { XCalendarNode } from './calendar.property';
@@ -47,8 +38,7 @@ import type { XI18nCalendar } from '@ng-nest/ui/i18n';
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [DatePipe, LowerCasePipe]
 })
-export class XCalendarComponent extends XCalendarProperty implements OnDestroy {
-  private _unSubject = new Subject<void>();
+export class XCalendarComponent extends XCalendarProperty {
   private datePipe = inject(DatePipe);
   private lowerCasePipe = inject(LowerCasePipe);
   private i18n = inject(XI18nService);
@@ -60,13 +50,9 @@ export class XCalendarComponent extends XCalendarProperty implements OnDestroy {
   ];
   modelSignal = signal(this.model());
 
-  locale = toSignal(
-    this.i18n.localeChange.pipe(
-      map((x) => x.calendar as XI18nCalendar),
-      takeUntil(this._unSubject)
-    ),
-    { initialValue: zh_CN.calendar }
-  );
+  locale = toSignal(this.i18n.localeChange.pipe(map((x) => x.calendar as XI18nCalendar)), {
+    initialValue: zh_CN.calendar
+  });
   radioData = computed(() => {
     return this.radioDataBase.map((x) => {
       x.label = (this.locale() as any)[x.id];
@@ -94,11 +80,6 @@ export class XCalendarComponent extends XCalendarProperty implements OnDestroy {
 
     return dt;
   });
-
-  ngOnDestroy() {
-    this._unSubject.next();
-    this._unSubject.unsubscribe();
-  }
 
   action(next: number) {
     let datetime = new Date(this.datetime().getTime());
