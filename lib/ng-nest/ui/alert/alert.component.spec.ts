4cbca2c07be367d9ba574906c1e77687b8796d8a
ng-nest-moon <ngnest@163.com>
2024-09-22 17:05:56 +0800

    test(module:alert): update spec test

--- lib/ng-nest/ui/alert/alert.component.spec.ts
+++ lib/ng-nest/ui/alert/alert.component.spec.ts
@@ -1,10 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Component, ElementRef, provideExperimentalZonelessChangeDetection, signal } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  provideExperimentalZonelessChangeDetection,
+  signal,
+  TemplateRef,
+  viewChild
+} from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { XAlertComponent, XAlertDragFreeDragPosition, XAlertPrefix, XAlertType } from '@ng-nest/ui/alert';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { XEffect, XTemplate } from '@ng-nest/ui/core';
+import { XEffect, XSleep, XTemplate } from '@ng-nest/ui/core';
 import { CdkDragEnd } from '@angular/cdk/drag-drop';
 import { XResizableEvent } from '@ng-nest/ui/resizable';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -45,12 +52,17 @@ class XTestAlertComponent {}
       (dragEnded)="dragEnded($event)"
       (resizing)="resizing($event)"
     ></x-alert>
+
+    <ng-template #titleTpl>titleTpl</ng-template>
+    <ng-template #contentTpl>contentTpl</ng-template>
   `
 })
 class XTestAlertPropertyComponent {
   hide = signal(false);
   title = signal<XTemplate>('');
+  titleTpl = viewChild.required<TemplateRef<void>>('titleTpl');
   content = signal<XTemplate>('');
+  contentTpl = viewChild.required<TemplateRef<void>>('contentTpl');
   type = signal<XAlertType>('info');
   effect = signal<XEffect>('light');
   hideClose = signal(false);
@@ -127,69 +139,179 @@ describe(XAlertPrefix, () => {
       expect(alert).toBeNull();
     });
     it('title.', () => {
-      expect(true).toBe(true);
+      const title = fixture.debugElement.query(By.css('.x-alert-title'));
+      expect(title.nativeElement.innerText).toBe('');
+
+      component.title.set('title');
+      fixture.detectChanges();
+      expect(title.nativeElement.innerText).toBe('title');
+
+      component.title.set(component.titleTpl());
+      fixture.detectChanges();
+      expect(title.nativeElement.innerText).toBe('titleTpl');
     });
     it('content.', () => {
-      expect(true).toBe(true);
+      let content = fixture.debugElement.query(By.css('.x-alert-content'));
+      expect(content).toBeNull();
+
+      component.content.set('content');
+      fixture.detectChanges();
+      content = fixture.debugElement.query(By.css('.x-alert-content'));
+      expect(content.nativeElement.innerText).toBe('content');
+
+      component.content.set(component.contentTpl());
+      fixture.detectChanges();
+      expect(content.nativeElement.innerText).toBe('contentTpl');
     });
     it('type.', () => {
-      expect(true).toBe(true);
+      let alert = fixture.debugElement.query(By.css('.x-alert'));
+      expect(alert.nativeElement).toHaveClass('x-alert-info');
+
+      component.type.set('success');
+      fixture.detectChanges();
+      expect(alert.nativeElement).toHaveClass('x-alert-success');
+
+      component.type.set('warning');
+      fixture.detectChanges();
+      expect(alert.nativeElement).toHaveClass('x-alert-warning');
+
+      component.type.set('error');
+      fixture.detectChanges();
+      expect(alert.nativeElement).toHaveClass('x-alert-error');
+
+      component.type.set('loading');
+      fixture.detectChanges();
+      expect(alert.nativeElement).toHaveClass('x-alert-loading');
     });
     it('effect.', () => {
-      expect(true).toBe(true);
+      let alert = fixture.debugElement.query(By.css('.x-alert'));
+      expect(alert.nativeElement).toHaveClass('x-light');
+
+      component.effect.set('dark');
+      fixture.detectChanges();
+      expect(alert.nativeElement).toHaveClass('x-dark');
+
+      component.effect.set('white');
+      fixture.detectChanges();
+      expect(alert.nativeElement).toHaveClass('x-white');
     });
     it('hideClose.', () => {
-      expect(true).toBe(true);
+      let alert = fixture.debugElement.query(By.css('.x-alert'));
+      expect(alert.nativeElement).toHaveClass('x-alert-has-close');
+
+      component.hideClose.set(true);
+      fixture.detectChanges();
+      expect(alert.nativeElement).not.toHaveClass('x-alert-has-close');
     });
     it('closeText.', () => {
-      expect(true).toBe(true);
+      let close = fixture.debugElement.query(By.css('.x-alert-close'));
+      expect(close.nativeElement.innerText).toBe('');
+
+      component.closeText.set('close');
+      fixture.detectChanges();
+      expect(close.nativeElement.innerText).toBe('close');
     });
     it('showIcon.', () => {
-      expect(true).toBe(true);
+      let icon = fixture.debugElement.query(By.css('.x-alert-icon'));
+      expect(icon).toBeNull();
+
+      component.showIcon.set(true);
+      fixture.detectChanges();
+      icon = fixture.debugElement.query(By.css('.x-alert-icon'));
+      expect(icon).toBeDefined();
     });
     it('disabledAnimation.', () => {
+      // angular animation disabled
       expect(true).toBe(true);
     });
-    it('duration.', () => {
-      expect(true).toBe(true);
+    it('duration.', async () => {
+      const com = fixture.debugElement.query(By.directive(XAlertComponent));
+      let close = com.query(By.css('.x-alert-operation-close'));
+      close.nativeElement.click();
+      fixture.detectChanges();
+      expect(com.componentInstance.styleHide()).toBe(true);
+    });
+    it('duration 300.', async () => {
+      component.duration.set(300);
+      const com = fixture.debugElement.query(By.directive(XAlertComponent));
+      let close = com.query(By.css('.x-alert-operation-close'));
+      close.nativeElement.click();
+      fixture.detectChanges();
+
+      component.duration.set(400);
+      await XSleep(400);
+      expect(com.componentInstance.styleHide()).toBe(true);
     });
     it('manual.', () => {
-      expect(true).toBe(true);
+      component.manual.set(true);
+      fixture.detectChanges();
+      const com = fixture.debugElement.query(By.directive(XAlertComponent));
+      let close = com.query(By.css('.x-alert-operation-close'));
+      close.nativeElement.click();
+      fixture.detectChanges();
+      expect(com.componentInstance.styleHide()).toBe(false);
     });
     it('draggable.', () => {
-      expect(true).toBe(true);
+      component.draggable.set(true);
+      fixture.detectChanges();
+      let alert = fixture.debugElement.query(By.css('.x-alert'));
+      expect(alert.nativeElement.classList).toContain('x-alert-draggable');
     });
     it('resizable.', () => {
+      // test resizable directive
       expect(true).toBe(true);
     });
     it('offsetLeft.', () => {
+      // test resizable directive
       expect(true).toBe(true);
     });
     it('offsetTop.', () => {
+      // test resizable directive
       expect(true).toBe(true);
     });
     it('minWidth.', () => {
-      expect(true).toBe(true);
+      component.minWidth.set('300px');
+      fixture.detectChanges();
+      let alert = fixture.debugElement.query(By.css('.x-alert'));
+      expect(alert.nativeElement.style.minWidth).toBe('300px');
     });
     it('minHeight.', () => {
-      expect(true).toBe(true);
+      component.minHeight.set('300px');
+      fixture.detectChanges();
+      let alert = fixture.debugElement.query(By.css('.x-alert'));
+      expect(alert.nativeElement.style.minHeight).toBe('300px');
     });
     it('dragBoundary.', () => {
+      // test cdkDrag
       expect(true).toBe(true);
     });
     it('dragFreeDragPosition.', () => {
+      // test cdkDrag
       expect(true).toBe(true);
     });
     it('operationTpl.', () => {
-      expect(true).toBe(true);
+      let operation = fixture.debugElement.query(By.css('.x-alert-operation'));
+      expect(operation.nativeElement.innerText).toBe('');
+
+      component.operationTpl.set('operationTpl string');
+      fixture.detectChanges();
+      expect(operation.nativeElement.innerText).toBe('operationTpl string');
     });
     it('close.', () => {
-      expect(true).toBe(true);
+      component.manual.set(true);
+      fixture.detectChanges();
+      const com = fixture.debugElement.query(By.directive(XAlertComponent));
+      let close = com.query(By.css('.x-alert-operation-close'));
+      close.nativeElement.click();
+      fixture.detectChanges();
+      expect(component.closed()).toBe(true);
     });
     it('dragEnded.', () => {
+      // test cdkDrag
       expect(true).toBe(true);
     });
     it('resizing.', () => {
+      // test resizable directive
       expect(true).toBe(true);
     });
   });
