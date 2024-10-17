ce9f03bb1c0b58128160a94bed6a3bd87f7f6c7f
ng-nest-moon <ngnest@163.com>
2024-10-17 20:42:21 +0800

    test(module:highlight): update test example

--- lib/ng-nest/ui/highlight/highlight.component.spec.ts
+++ lib/ng-nest/ui/highlight/highlight.component.spec.ts
@@ -1,9 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Component, provideExperimentalZonelessChangeDetection, signal } from '@angular/core';
+import { Component, ElementRef, provideExperimentalZonelessChangeDetection, signal, viewChild } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { XHighlightComponent, XHighlightLines, XHighlightPrefix } from '@ng-nest/ui/highlight';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 @Component({
   standalone: true,
@@ -22,6 +23,8 @@ class XTestHighlightComponent {}
       [highlightLines]="highlightLines()"
       [showCopy]="showCopy()"
     ></x-highlight>
+
+    <input #input allow="clipboard-write" />
   `
 })
 class XTestHighlightPropertyComponent {
@@ -29,6 +32,7 @@ class XTestHighlightPropertyComponent {
   data = signal('');
   highlightLines = signal<XHighlightLines>({});
   showCopy = signal(false);
+  input = viewChild.required<ElementRef<HTMLInputElement>>('input');
 }
 
 describe(XHighlightPrefix, () => {
@@ -36,10 +40,12 @@ describe(XHighlightPrefix, () => {
     TestBed.configureTestingModule({
       imports: [XTestHighlightComponent, XTestHighlightPropertyComponent],
       providers: [
+        provideAnimations(),
         provideHttpClient(withInterceptorsFromDi()),
         provideHttpClientTesting(),
         provideExperimentalZonelessChangeDetection()
-      ]
+      ],
+      teardown: { destroyAfterEach: false }
     }).compileComponents();
   });
   describe('default.', () => {
@@ -55,23 +61,52 @@ describe(XHighlightPrefix, () => {
   });
   describe(`input.`, async () => {
     let fixture: ComponentFixture<XTestHighlightPropertyComponent>;
-    // let component: XTestHighlightPropertyComponent;
+    let component: XTestHighlightPropertyComponent;
     beforeEach(async () => {
       fixture = TestBed.createComponent(XTestHighlightPropertyComponent);
-      // component = fixture.componentInstance;
+      component = fixture.componentInstance;
       fixture.detectChanges();
     });
     it('type.', () => {
-      expect(true).toBe(true);
+      component.type.set('html');
+      fixture.detectChanges();
+      const code = fixture.debugElement.query(By.css('.x-highlight > pre > code'));
+      expect(code.nativeElement).toHaveClass('language-html');
     });
     it('data.', () => {
-      expect(true).toBe(true);
+      const html = '<span class="x-highlight"></span>';
+      component.data.set(html);
+      component.type.set('html');
+      fixture.detectChanges();
+      const code = fixture.debugElement.query(By.css('.x-highlight > pre > code'));
+      expect(code.nativeElement).toHaveClass('language-html');
+      expect(code.nativeElement.innerText).toBe(html);
     });
     it('highlightLines.', () => {
-      expect(true).toBe(true);
+      const html = `<span class="x-highlight"></span>
+<span class="x-highlight"></span>`;
+      component.data.set(html);
+      component.highlightLines.set({ danger: '1', primary: '2' });
+      component.type.set('html');
+      fixture.detectChanges();
+      const lines = fixture.debugElement.queryAll(By.css('.line-highlight'));
+      expect(lines.length).toBe(2);
+      expect(lines[0].nativeElement).toHaveClass('danger');
+      expect(lines[1].nativeElement).toHaveClass('primary');
     });
-    it('showCopy.', () => {
-      expect(true).toBe(true);
+    it('showCopy.', async () => {
+      const html = '<span class="x-highlight"></span>';
+      component.showCopy.set(true);
+      component.data.set(html);
+      component.type.set('html');
+      fixture.detectChanges();
+      const copy = fixture.debugElement.query(By.css('.x-highlight-copy'));
+      expect(copy).toBeTruthy();
+      copy.nativeElement.click();
+      component.input().nativeElement.focus();
+      fixture.detectChanges();
+      const text = await navigator.clipboard.readText();
+      expect(text).toBe(html);
     });
   });
 });
