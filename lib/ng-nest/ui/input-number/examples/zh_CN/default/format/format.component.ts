377254a65f6d5439e8051a080d50e60a7179955f
ng-nest-moon <ngnest@163.com>
2025-05-21 21:41:03 +0800

    fix(module:input-number): fix format error

--- lib/ng-nest/ui/input-number/examples/zh_CN/default/format/format.component.ts
+++ lib/ng-nest/ui/input-number/examples/zh_CN/default/format/format.component.ts
@@ -1,15 +1,23 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { XNumber } from '@ng-nest/ui/core';
 import { XInputNumberComponent } from '@ng-nest/ui/input-number';
 
 @Component({
   selector: 'ex-format',
   standalone: true,
-  imports: [XInputNumberComponent],
+  imports: [XInputNumberComponent, FormsModule],
   templateUrl: './format.component.html',
   styleUrls: ['./format.component.scss']
 })
 export class ExFormatComponent {
+  model1 = signal(null);
+  model2 = signal(null);
+
   formatterDollar = signal((value: number): XNumber => (value ? `$ ${value}` : ''));
   formatterPercent = signal((value: number): XNumber => (value ? `${value} %` : ''));
+
+  change(value: number) {
+    console.log(value);
+  }
 }
