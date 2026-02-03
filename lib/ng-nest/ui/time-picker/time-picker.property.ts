5bdd40b4320251f08129300201d6101ee4863118
ng-nest-moon <ngnest@163.com>
2026-02-03 22:17:40 +0800

    fix(module:time-picker): fix pattern error

--- lib/ng-nest/ui/time-picker/time-picker.property.ts
+++ lib/ng-nest/ui/time-picker/time-picker.property.ts
@@ -188,12 +188,12 @@ export class XTimePickerProperty extends XFormControlFunction(X_TIME_PICKER_CONF
    * @zh_CN 正则验证规则
    * @en_US Regular verification rules
    */
-  override readonly pattern = input<RegExp | RegExp[] | any>(null);
+  override readonly pattern = input<RegExp | null>(null);
   /**
    * @zh_CN 验证不通过提示文字
    * @en_US Verify not pass the prompt text
    */
-  override readonly message = input<string | string[]>([]);
+  override readonly message = input<string>('');
   /**
    * @zh_CN 激活状态
    * @en_US Activation state
@@ -369,12 +369,12 @@ export interface XTimePickerOption extends XFormOption {
    * @zh_CN 正则验证规则
    * @en_US Regular verification rules
    */
-  pattern?: RegExp | RegExp[];
+  pattern?: RegExp;
   /**
    * @zh_CN 验证不通过提示文字
    * @en_US Verify not pass the prompt text
    */
-  message?: string | string[];
+  message?: string;
   /**
    * @zh_CN 激活状态
    * @en_US Activation state
